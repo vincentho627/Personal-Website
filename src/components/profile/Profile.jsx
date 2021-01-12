939f9bbb69469dbed32af687f4b5efe3ea1e0b79
Vincent Ho <kch19@ic.ac.uk>
2021-01-13 00:53:46 +0800

    added open in new tab instead of redirect

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -6,9 +6,9 @@ function Profile() {
         <div className="welcome">
           <h2>Ho Kwan Chak Vincent</h2>
           <p className="welcome_desc">Computing Student At Imperial College London</p>
-          <a href="https://www.facebook.com/vincent.ho.79462815/"><i className="bx nav__icon bxl-facebook-circle"></i></a>
-          <a href="https://github.com/vincentho627"><i className="bx nav__icon bxl-github"></i></a>
-          <a href="https://www.linkedin.com/in/vincent-ho-1099a3195/"><i className="bx nav__icon bxl-linkedin-square"></i></a>
+          <a href="https://www.facebook.com/vincent.ho.79462815/" target="_blank"><i className="bx nav__icon bxl-facebook-circle"></i></a>
+          <a href="https://github.com/vincentho627" target="_blank"><i className="bx nav__icon bxl-github"></i></a>
+          <a href="https://www.linkedin.com/in/vincent-ho-1099a3195/" target="_blank"><i className="bx nav__icon bxl-linkedin-square"></i></a>
         </div>
         <img className="img-responsive rounded-circle profile_pic" src={process.env.PUBLIC_URL + "/images/profile-pic.png"} alt=""/>
       </div>
--- src/components/projects/ProjectDescription.jsx
+++ src/components/projects/ProjectDescription.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ProjectDescription(props) {
   return (
-    <a href={props.url}>
+    <a href={props.url} target="_blank">
       <div className="overlay">
         <h2 className="project_description">{props.description}</h2>
       </div>
