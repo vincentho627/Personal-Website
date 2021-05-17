8a8f7854d98bc4654f361bd63b2cd5d9d3a54262
Vincent <kch19@ic.ac.uk>
2021-05-17 17:51:26 +0800

    new website

---
+++ src/components/navbar/navbarList.js
@@ -0,0 +1,24 @@
+const navbarList = [
+  {
+    id: 1,
+    name: "About",
+    link: "#about"
+  },
+  {
+    id: 2,
+    name: "Experience",
+    link: "#experiences"
+  },
+  {
+    id: 3,
+    name: "Projects",
+    link: "#projects"
+  },
+  {
+    id: 4,
+    name: "Contact",
+    link: "#contact"
+  }
+]
+
+export default navbarList;
