f6dec9664b6d4c30c2bd7f9b52d8ad1ab6e5b9ae
Vincent <kch19@ic.ac.uk>
2021-06-26 20:59:21 +0800

    fixed contact list

--- src/components/contacts/contact.jsx
+++ src/components/contacts/contact.jsx
@@ -6,10 +6,10 @@ function Contact() {
         <div className="contacts">
           <h2 className="header contact-header">Contact Me</h2>
           <div className="contacts-list">
-            <a className="contact-icon" href="https://www.facebook.com/vincent.ho.79462815"><i className='bx icons bxl-facebook-circle'></i></a>
-            <a className="contact-icon" href="https://github.com/vincentho627"><i className='bx icons bxl-github' ></i></a>
-            <a className="contact-icon" href="https://www.linkedin.com/in/vincent-ho-1099a3195/"><i className='bx icons'><img className="linkedin" src={process.env.PUBLIC_URL + "/img/linkedin.png"}/></i></a>
-            <a className="contact-icon" href="mailto:vincentkcho627@gmail.com"><i className='bx icons' ><img className="mail" src={process.env.PUBLIC_URL + "/img/mail.webp"}/></i></a>
+            <a className="contact-icon" href="https://www.facebook.com/vincent.ho.79462815"><img className="icons" src={process.env.PUBLIC_URL + "/img/facebook.png"}/></a>
+            <a className="contact-icon" href="https://github.com/vincentho627"><img className="icons" src={process.env.PUBLIC_URL + "/img/github.png"}/></a>
+            <a className="contact-icon" href="https://www.linkedin.com/in/vincent-ho-1099a3195/"><img className="icons" src={process.env.PUBLIC_URL + "/img/linkedin.png"}/></a>
+            <a className="contact-icon" href="mailto:vincentkcho627@gmail.com"><img className="icons" src={process.env.PUBLIC_URL + "/img/email.png"}/></a>
           </div>
           <div className="bye-gif">
             <img className="gif" src={process.env.PUBLIC_URL + "/img/bye.gif"} />
